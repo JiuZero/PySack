ab80b0edd9c3b7e9b6649c6b328c23b590984e1d
JiuZero <1703417187@qq.com>
2026-08-16 18:56:15 +0800

    Fix install command for test PyPI, bump to 0.2.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysack",
-    version="0.2.7",
+    version="0.2.8",
     license="MIT",
     description=(
         "Encrypt Python projects with Nuitka and pack with PyInstaller "
